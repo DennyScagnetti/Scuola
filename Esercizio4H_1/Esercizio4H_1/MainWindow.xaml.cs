2a78441395bfc2b8b35ce7c455edb2bf9f80005f
DennyScagnetti <denny.scagnetti@studenti.unicam.it>
2021-03-13 19:45:00 +0100

    prima parte
    Checkbox, butto, radiobox e eventi correlati

--- Esercizio4H_1/Esercizio4H_1/MainWindow.xaml.cs
+++ Esercizio4H_1/Esercizio4H_1/MainWindow.xaml.cs
@@ -13,16 +13,96 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Esercizio4H_1
+namespace Nuovo_Progetto
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logica di interazione per MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+
+        private void Evento_Bottone(object sender, RoutedEventArgs e)
+        {
+            double numInserito = Double.Parse(textInput.Text);
+            double numeroFinale = numInserito * 10;
+            textOutput.Text = numeroFinale.ToString();
+            textOutput.Text = Convert.ToString(numeroFinale);
+        }
+
+        private void pnlMainGrid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Hai cliccato " + e.GetPosition(this).ToString());
+        }
+
+        private void pnlMainGrid_Mousewheel(object sender, MouseWheelEventArgs e)
+        {
+
+            MessageBox.Show(this.Background.ToString());
+        }
+
+        private void ChiusuraFinestra(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+
+        private void ControllaSelezione(object sender, RoutedEventArgs e)
+        {
+            String scelta = "";
+            if (Opz1.IsChecked == true)
+            {
+                scelta = scelta + Opz1.Content + " ";
+            }
+            if (Opz2.IsChecked == true)
+            {
+                scelta = scelta + Opz2.Content + " ";
+            }
+            if (Opz3.IsChecked == true)
+            {
+                scelta = scelta + Opz3.Content;
+            }
+
+            if (scelta.Equals(""))
+            {
+                MessageBox.Show("Non hai selezionato niente");
+            }
+            else
+            {
+                MessageBox.Show("Hai selezionanto " + scelta);
+            }
+        }
+
+        private void Opz1_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+        int cont = 0;
+        private void checkR3(object sender, RoutedEventArgs e)
+        {
+            /*   cont++;
+               TxtRB.Text =  cont.ToString();*/
+        }
+
+        private void checkR2(object sender, RoutedEventArgs e)
+        {
+            TxtRB.Text = "Hai scelto la seconda opzione";
+        }
+
+        private void checkR1(object sender, RoutedEventArgs e)
+        {
+            TxtRB.Text = "Hai scelto la prima opzione";
+        }
+
+        private void clickR3(object sender, RoutedEventArgs e)
+        {
+            cont++;
+            TxtRB.Text = cont.ToString();
         }
     }
 }
