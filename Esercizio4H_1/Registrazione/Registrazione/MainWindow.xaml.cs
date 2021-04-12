ef0355ec333a3b3467121f98297b83c64a0c913c
DennyScagnetti <denny.scagnetti@studenti.unicam.it>
2021-04-12 11:45:23 +0200

    Form Registrazione

---
+++ Esercizio4H_1/Registrazione/Registrazione/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Registrazione
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string elenco_campi = "";
+            if (Nome.Text == "") { 
+                elenco_campi = elenco_campi + "Nome \n";
+            }
+            if (Cognome.Text == "")
+            {
+                elenco_campi = elenco_campi + "Cognome \n";
+            }
+            if (Email.Text == "")
+            {
+                elenco_campi = elenco_campi + "Email \n";
+            }
+            if (Password.Password == "")
+            {
+                elenco_campi = elenco_campi + "Password \n";
+            }
+            if (Privacy.IsChecked == false)
+            {
+                elenco_campi = elenco_campi + "Privacy";
+            }
+            if(elenco_campi == "")
+            {
+                elenco_campi = "Registrazione effettuata correttamente";
+                Utente U = new Utente(Nome.Text, Cognome.Text, Email.Text, Password.Password,(bool)Privacy.IsChecked);
+            }
+            else
+            {
+                elenco_campi = "Errore nella registrazione. Mancano i seguenti campi:\n" + elenco_campi;
+            }
+            MessageBox.Show(elenco_campi);
+        }
+
+        private void Sesso_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(Sesso.SelectedIndex == 2)
+            {
+                LabelAltroSesso.IsEnabled = true;
+                TextAltroSesso.IsEnabled = true;
+            }
+            else
+            {
+                LabelAltroSesso.IsEnabled = false;
+                TextAltroSesso.IsEnabled = false;
+            }
+            
+        }
+
+        private void TextAltroSesso_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Sesso.Items.Count == 4)
+            {
+                Sesso.Items.RemoveAt(3);
+            }
+            Sesso.Items.Add(TextAltroSesso.Text);
+            Sesso.Text = TextAltroSesso.Text;
+            Sesso.SelectedIndex = Sesso.Items.Count - 1;
+            if(Sesso.Items.Count == 4 && Sesso.Items.IndexOf("") == 3)
+            {
+                Sesso.Items.RemoveAt(3);
+            }
+        }
+
+    private Boolean controllaEmail(string email)
+        {
+            Boolean b = true;
+            if (!email.Contains("@") || email.IndexOf('@')==0 || email.LastIndexOf('.') <= (email.IndexOf('@') + 2) || email.LastIndexOf('.')>=email.Length-2)
+                b = false;
+            return b;
+        }
+
+        private void Email_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (!controllaEmail(Email.Text))
+            {
+                MessageBox.Show("Email non corretta");
+            }
+        }
+    }
+}
---
+++ Esercizio4H_1/Registrazione/Registrazione/Utente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registrazione
+{
+    class Utente
+    {
+        string nome;
+        string cognome;
+        string email;
+        string password;
+        bool privacy;
+        public Utente(string nome, string cognome, string email, string password, bool privacy)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.email = email;
+            this.password = password;
+            this.privacy = privacy;
+        }
+        
+    }
+}
