e7e9e77d942a69323e6464a154f429882448ed02
DennyScagnetti <denny.scagnetti@studenti.unicam.it>
2021-05-03 16:25:49 +0200

    Binding

--- Esercizio4H_1/CompitoAprile/CompitoAprile/MainWindow.xaml.cs
+++ Esercizio4H_1/CompitoAprile/CompitoAprile/MainWindow.xaml.cs
@@ -24,19 +24,45 @@ namespace CompitoAprile
         {
             InitializeComponent();
         }
-
+        
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (/*txt2.Text == "rispondi qui"*/false)
+            bool risp = true;
+            string risp_manc = "";
+            if (e1_1.IsChecked == false && e1_2.IsChecked == false && e1_3.IsChecked == false && c1.IsChecked == false)
             {
-                MessageBox.Show("Hai tralasciato la domanda 2!!");
-
-
+                risp = false;
+                risp_manc += " 1";
             }
-            else { 
+
+            if (txt2.Text == "rispondi qui" || txt2.Text == "")
+            {
+                risp = false;
+                risp_manc += " 2";
+            }
+            
+            if (e3_1.IsChecked == false && e3_2.IsChecked == false && c2.IsChecked == false)
+            {
+                risp = false;
+                risp_manc += " 3";
+            }
+            if (risp== true){ 
             Parte2 P = new Parte2();
             P.Show();
-            this.Close();
+                int tot = 0;
+                if (c1.IsChecked == true)
+                    tot++;
+                if (txt2.Text == "20")
+                    tot++;
+                if (c2.IsChecked == true)
+                    tot++;
+                P.conteggio.Text =Convert.ToString(tot);
+
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Hai tralasciato le seguenti risposte: " + risp_manc,"Risposte mancanti",MessageBoxButton.OKCancel, MessageBoxImage.Warning, MessageBoxResult.OK);
             }
         }
 
--- Esercizio4H_1/CompitoAprile/CompitoAprile/Parte2.xaml.cs
+++ Esercizio4H_1/CompitoAprile/CompitoAprile/Parte2.xaml.cs
@@ -26,11 +26,57 @@ namespace CompitoAprile
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if(chk1.IsChecked == false)
-            {
-                MessageBox.Show("Hai tralasciato la domanda 6!!");
 
-                
+            bool risp = true;
+            string risp_manc = "";
+            if (e4_1.IsChecked == false && c4_1.IsChecked == false && c4_2.IsChecked == false && c4_3.IsChecked == false)
+            {
+                risp = false;
+                risp_manc += " 4";
+            }
+
+            if (e5.IsChecked == false && c5.IsChecked == false)
+            {
+                risp = false;
+                risp_manc += " 5";
+            }
+
+            if (e6_1Iso.IsChecked == false && e6_2Iso.IsChecked == false && c6_1Iso.IsChecked == false && c6_2Iso.IsChecked == false && c6_3Iso.IsChecked == false)
+            {
+                risp = false;
+                risp_manc += " 6 (prima riga)";
+            }
+            if (c1_1Ret.IsChecked == false && c1_2Ret.IsChecked == false && c2_1Ret.IsChecked == false && c2_2Ret.IsChecked == false && c12Ret.IsChecked == false)
+            {
+                risp = false;
+                risp_manc += " 6 (seconda riga)";
+            }
+            if (e6_1Sca.IsChecked == false && e6_2Sca.IsChecked == false && c6_1Sca.IsChecked == false && c6_2Sca.IsChecked == false && c6_3Sca.IsChecked == false)
+            {
+                risp = false;
+                risp_manc += " 6 (terza riga)";
+            }
+            if (risp == true)
+            {
+                int tot = Convert.ToInt32(conteggio.Text);
+                if (c4_1.IsChecked == true && c4_2.IsChecked == true && c4_3.IsChecked == true)
+                    tot += 2;
+                if (c5.IsChecked == true)
+                    tot += 2;
+                if (c6_1Iso.IsChecked == true && c6_2Iso.IsChecked == true && c6_3Iso.IsChecked == true)
+                    tot += 1;
+                if (c6_1Sca.IsChecked == true && c6_2Sca.IsChecked == true && c6_3Sca.IsChecked == true)
+                    tot += 1;
+                if ((c1_1Ret.IsChecked == true && c1_2Ret.IsChecked == true && c12Ret.IsChecked == true) || (c2_1Ret.IsChecked == true && c2_2Ret.IsChecked == true && c12Ret.IsChecked == true))
+                    tot += 1;
+                MessageBox.Show("Hai fatto "+tot);
+            }
+            else
+            {
+               
+                MessageBoxResult MR = MessageBox.Show("Hai tralasciato le seguenti risposte: " + risp_manc, "Risposte mancanti", MessageBoxButton.OKCancel, MessageBoxImage.Warning, MessageBoxResult.OK);
+                if (MR == MessageBoxResult.OK)
+                    MessageBox.Show("Stai più attento");
             }
         }
     }
